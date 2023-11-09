8dae5681d552a6f53ffd02973923b3c5ac5ba40f
bricklayerrrr <bricklayerrrr@gmail.com>
2023-11-09 19:01:05 +0900

    clean library

--- contracts/vcnote/VCNote.sol
+++ contracts/vcnote/VCNote.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.20;
 
-import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import {VCNoteStorage, VCNoteStorageLib} from "./storages/VCNoteStorage.sol";
 import {BorrowPermitParams} from "./libraries/BorrowPermitParams.sol";
 
