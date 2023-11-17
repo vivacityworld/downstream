1f7823fcc4f38edf9ff88967278486f112b94213
bricklayerrrr <bricklayerrrr@gmail.com>
2023-11-17 16:33:32 +0900

    update vcnote router event

--- contracts/vcnote/VCNoteRouter.sol
+++ contracts/vcnote/VCNoteRouter.sol
@@ -17,11 +17,10 @@ import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol
 contract VCNoteRouter is ReentrancyGuard {
     using Math for uint256;
 
-    event Mint(address indexed user, uint256 inputNote, uint256 outputVCNote);
-    event Redeem(address indexed user, uint256 inputVCNote, uint256 outputNote);
-    event Borrow(address indexed user, uint256 outputNote);
-    event RepayBorrow(address indexed user, uint256 inputNote, uint256 repaidCNote);
-    event LiquidateBorrow(address indexed user, uint256 inputNote, uint256 repaidCNote, address collateral, uint256 outputCollateral);
+    event Mint(address minter, uint mintAmount, uint mintTokens);
+    event Redeem(address redeemer, uint redeemAmount, uint redeemTokens);
+    event Borrow(address borrower, uint256 borrowAmount);
+    event RepayBorrow(address payer, uint repayAmount);
 
     IVCNote public immutable vcNOTE;
     ICERC20 public immutable cNOTE;
@@ -116,7 +115,7 @@ contract VCNoteRouter is ReentrancyGuard {
         // repayBorrow
         vcNOTE.repayBorrowBehalf(msg.sender, mintedCNote);
 
-        emit RepayBorrow(msg.sender, amount, mintedCNote);
+        emit RepayBorrow(msg.sender, amount);
     }
 
     /**
